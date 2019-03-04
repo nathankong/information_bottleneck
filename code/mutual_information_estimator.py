Fixed MI computation, added MI evolution plot.

code/mutual_information_estimator.py
@@ -19,16 +19,18 @@ class MutualInformationEstimator():
         self.noise_distribution = noise_distribution
         self.data_distribution = data_distribution
         self.input_dataset = input_dataset
-        self.tol = 1e-7
+        self.tol = 1e-10
 
     def compute_mutual_information(self, layer_samples, num_samples_per_outcome):
         uncond_entropy = self.compute_unconditional_entropy(layer_samples)
+
+        #print "Unconditional entropy:", uncond_entropy
 
         mutual_information = uncond_entropy
         for i in range(self.input_dataset.shape[0]):
             curr_outcome = self.input_dataset[i]
             cond_entr = self.compute_conditional_entropy(curr_outcome, num_samples_per_outcome)
-            print cond_entr
+            #print "Conditional entropy sample {}: {}".format(curr_outcome, cond_entr)
             cond_entropy = self.data_distribution(curr_outcome) * cond_entr
             mutual_information -= cond_entropy
 
@@ -44,10 +46,10 @@ class MutualInformationEstimator():
         # p_{T_\ell | X = x_i} = p_{S_\ell | X = x_i} \ast \phi \approx \hat{p}_{S_\ell}^{(i)} \ast \phi
         estimated_conditional_distribution = lambda x: (1./num_samples_per_outcome)* \
                                                 np.sum(self.noise_distribution(x - model_outputs_noise))
-        integrand = lambda x: estimated_conditional_distribution(x) * np.log(1./estimated_conditional_distribution(x) + self.tol)
+        integrand = lambda x: -1. * estimated_conditional_distribution(x) * np.log(estimated_conditional_distribution(x) + self.tol)
 
         # SP estimator: h(p_{T_\ell | X = x_i}) \approx h(\hat{p}_{S_{\ell}^{(i)}} \ast \phi)
-        conditional_entropy, _ = integrate.quad(integrand, -np.inf, np.inf)
+        conditional_entropy, _ = integrate.quad(integrand, -1000, 1000)
 
         return conditional_entropy
 
@@ -64,11 +66,10 @@ class MutualInformationEstimator():
         estimated_distribution = lambda x: (1./num_samples)*np.sum(self.noise_distribution(x - samples))
 
         # Integrand for computing the entropy. i.e. f(x) log (1/f(x))
-        integrand = lambda x: estimated_distribution(x) * np.log(1./estimated_distribution(x) + self.tol)
+        integrand = lambda x: -1. * estimated_distribution(x) * np.log(estimated_distribution(x) + self.tol)
 
         # SP estimator: h(p_{T_\ell}) \approx h(\hat{p}_{S_\ell} \ast \phi)
-        unconditional_entropy, _ = integrate.quad(integrand, -np.inf, np.inf)
+        unconditional_entropy, _ = integrate.quad(integrand, -1000, 1000)
 
         return unconditional_entropy
-
 

code/train.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--beta', type=float, default=0.05)
     parser.add_argument('--noise', type=bool, default=False)
-    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--lr', type=float, default=0.001)
     args = parser.parse_args()
 
     # Use GPU or not
@@ -111,7 +111,6 @@ if __name__ == "__main__":
     mutual_info = np.zeros((args.epochs,))
     for i in xrange(args.epochs):
         if (i+1) % 1 == 0:
-            print("Epoch", i+1)
             m.eval()
             acc, test_out_noise, test_y = test_model(m, test_loader)
             accs[i] = acc
@@ -124,6 +123,8 @@ if __name__ == "__main__":
             # Compute MI
             curr_mutual_info = mi.compute_mutual_information(gen_outputs.detach().cpu().numpy(), 500)
             mutual_info[i] = curr_mutual_info
+
+            print("Epoch {}; MI {}; Acc {}".format(i+1, curr_mutual_info, acc))
 
         m.train()
         for batch_idx, (data_x, data_y) in enumerate(train_loader):