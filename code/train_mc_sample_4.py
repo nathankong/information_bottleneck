1477466ec5cb6aff35eeccc2e92699c049905f6f
Nathan Kong <kongnathan@gmail.com>
2019-03-09 15:03:57 -0800

    Visualize new result from MC sampling. Changed MC train script name. Added new train script for MC sampling in the two neuron relu example.

---
+++ code/train_mc_sample_4.py
@@ -0,0 +1,176 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch import optim
+
+from dataset import ScalarDataset, build_dataset
+from model import Model, NoiseModel
+from mutual_information_estimator_continuous import MutualInformationEstimator
+from utils import UnivariateGaussian, UniformDataDistribution
+
+
+def sample(m, device, num_samp=1000):
+    X, _ = build_dataset(num_samp)
+    output_noise, output = m(torch.from_numpy(X).float().to(device))
+    return output_noise, output
+
+
+def test_model(m, test_loader, device):
+    avg_acc = 0
+    num_batch = 0
+    for _, (test_x, test_y) in enumerate(test_loader):
+        test_out_noise, _ = m(test_x.to(device))
+        avg_acc += compute_acc(test_out_noise.detach().cpu().numpy(), test_y.detach().cpu().numpy())
+        num_batch += 1
+
+    avg_acc /= num_batch
+    assert num_batch == 1
+
+    return avg_acc, test_out_noise, test_y
+
+
+def compute_acc(model_out, true_out):
+    # model_out = tanh values
+    assert model_out.shape[0] == true_out.shape[0]
+    model_out[model_out<=0] = -1
+    model_out[model_out>0] = 1
+    correct = np.sum((model_out==true_out).astype(int))
+    return correct / true_out.shape[0]
+
+
+if __name__ == "__main__":
+    # python train_mc_sample.py --epochs 200 --beta 0.05 --noise True --lr 0.001
+
+    # Sample size
+    N = 30
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=1000)
+    parser.add_argument('--batch_size', type=int, default=25)
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--beta', type=float, default=0.05)
+    parser.add_argument('--noise', type=bool, default=False)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--results_dir', type=str, default="")
+    args = parser.parse_args()
+
+    # Results dir
+    if args.results_dir == "":
+        assert 0, "Need results directory."
+    if not os.path.exists(args.results_dir):
+        os.makedirs(args.results_dir)
+    print("Results directory:", args.results_dir)
+
+    # Use GPU or not
+    cuda = torch.cuda.is_available()
+    #cuda = False
+    device = torch.device('cuda' if cuda else 'cpu')
+    print("Using cuda:", cuda)
+    print("Device:", device)
+    # Noise or no noise
+    print("Using noise model:", args.noise)
+
+    # Reproducibility
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    # Learning rate
+    print("Learning rate:", args.lr)
+
+    # Datasets
+    train_dataset = ScalarDataset(N, test=False)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False)
+
+    test_dataset = ScalarDataset(N, test=False)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=N, shuffle=False)
+
+    # Model
+    if not args.noise:
+        m = Model() # Default dim=1
+    else:
+        m = NoiseModel(beta=args.beta) # Add noise to layer
+    m = m.to(device)
+
+    # Optimizer
+    optimizer = optim.SGD(m.parameters(), lr=args.lr)
+    loss_func = nn.MSELoss()
+
+    # Set up probability distributions
+    noise_distr = UnivariateGaussian(0, np.square(args.beta))
+    # The '4' is hard coded since the dataset is {-3,-1,1,3}
+    data_distr = UniformDataDistribution(4, outcomes=np.array([-3,-1,1,3]).reshape(4,1))
+
+    # Mutual information estimator
+    mi = MutualInformationEstimator(
+        m,
+        torch.device("cpu"),
+        noise_distr.compute_probability,
+        data_distr.compute_probability,
+        np.array([-3,-1,1,3]).reshape(4,1)
+    )
+
+    # Start the training
+    accs = np.zeros((args.epochs,))
+    mutual_info = np.zeros((args.epochs,))
+    for i in xrange(args.epochs):
+        if (i+1) % 1 == 0:
+            # Convert to CPU
+            m.eval()
+            m.to(torch.device("cpu"))
+            acc, test_out_noise, test_y = test_model(m, test_loader, torch.device("cpu"))
+            accs[i] = acc
+
+            # Get noise samples
+            gen_noise_outputs, gen_outputs = sample(m, torch.device("cpu"), num_samp=1000)
+            np.save(args.results_dir + "/epoch_{}_outputs_noise.npy".format(i+1), gen_noise_outputs.detach().cpu().numpy())
+
+            # Compute MI
+            curr_mutual_info = mi.compute_mutual_information(
+                gen_outputs["output"].detach().cpu().numpy(),
+                1000,
+                "output",
+                data_distribution_sampler=data_distr.sample,
+                num_mc_samples=100
+            )
+            mutual_info[i] = curr_mutual_info
+
+            print("Epoch {}; MI {}; Acc {}".format(i+1, curr_mutual_info, acc))
+
+        # Convert back to original device
+        m.train()
+        m.to(device)
+        for batch_idx, (data_x, data_y) in enumerate(train_loader):
+            data_x, data_y = data_x.to(device), data_y.to(device) 
+
+            output_noise, _ = m(data_x)
+            loss = loss_func(output_noise, data_y)
+            loss.backward()
+            optimizer.step()
+
+
+    # Diagnostics
+    for param in m.parameters():
+        print("Weight:", param.data)
+
+    new_set_x, new_set_y = build_dataset(10)
+    model_y = np.tanh(6.9393*new_set_x - 14.2929)
+    print("True out:", new_set_y.T)
+    print("Model out:",  model_y.T)
+    new_set_x, new_set_y = build_dataset(10)
+    print("Random out:", new_set_x.T)
+    print("Random out label:", new_set_y.T)
+
+    # Save stuff
+    np.save(args.results_dir + "/accuracies.npy", accs)
+    np.save(args.results_dir + "/mutual_information.npy", mutual_info)
+
+
+
--- code/train_mc_sample_8.py
+++ code/train_mc_sample_8.py
@@ -10,16 +10,16 @@ import torch
 import torch.nn as nn
 from torch import optim
 
-from dataset import ScalarDataset, build_dataset
-from model import Model, NoiseModel
+from dataset import ScalarDatasetEight, build_dataset_eight
+from model import NoiseModelReLU
 from mutual_information_estimator_continuous import MutualInformationEstimator
 from utils import UnivariateGaussian, UniformDataDistribution
 
 
 def sample(m, device, num_samp=1000):
-    X, _ = build_dataset(num_samp)
-    output_noise, output = m(torch.from_numpy(X).float().to(device))
-    return output_noise, output
+    X, _ = build_dataset_eight(num_samp)
+    output_noise, output_dict = m(torch.from_numpy(X).float().to(device))
+    return output_noise, output_dict
 
 
 def test_model(m, test_loader, device):
@@ -37,19 +37,18 @@ def test_model(m, test_loader, device):
 
 
 def compute_acc(model_out, true_out):
-    # model_out = tanh values
     assert model_out.shape[0] == true_out.shape[0]
-    model_out[model_out<=0] = -1
-    model_out[model_out>0] = 1
+    model_out[model_out<=0.125] = 0
+    model_out[model_out>0.125] = 0.25
     correct = np.sum((model_out==true_out).astype(int))
     return correct / true_out.shape[0]
 
 
 if __name__ == "__main__":
-    # python train_mc_sample.py --epochs 200 --beta 0.05 --noise True --lr 0.001
+    # python information_bottleneck/code/train_eight.py --epochs 300 --batch_size 100 --noise True --beta 0.05 --lr 0.0001
 
     # Sample size
-    N = 30
+    N = 100
 
     import argparse
     parser = argparse.ArgumentParser()
@@ -71,7 +70,6 @@ if __name__ == "__main__":
 
     # Use GPU or not
     cuda = torch.cuda.is_available()
-    #cuda = False
     device = torch.device('cuda' if cuda else 'cpu')
     print("Using cuda:", cuda)
     print("Device:", device)
@@ -86,17 +84,17 @@ if __name__ == "__main__":
     print("Learning rate:", args.lr)
 
     # Datasets
-    train_dataset = ScalarDataset(N, test=False)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False)
+    train_dataset = ScalarDatasetEight(N)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
 
-    test_dataset = ScalarDataset(N, test=False)
+    test_dataset = ScalarDatasetEight(N)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=N, shuffle=False)
 
     # Model
     if not args.noise:
-        m = Model() # Default dim=1
+        assert 0, "Should only run noise model."
     else:
-        m = NoiseModel(beta=args.beta) # Add noise to layer
+        m = NoiseModelReLU(beta=args.beta) # Add noise to layer
     m = m.to(device)
 
     # Optimizer
@@ -105,8 +103,8 @@ if __name__ == "__main__":
 
     # Set up probability distributions
     noise_distr = UnivariateGaussian(0, np.square(args.beta))
-    # The '4' is hard coded since the dataset is {-3,-1,1,3}
-    data_distr = UniformDataDistribution(4, outcomes=np.array([-3,-1,1,3]).reshape(4,1))
+    # The '8' is hard coded since the dataset is {1,2,3,4,5,6,7,8}
+    data_distr = UniformDataDistribution(8, outcomes=np.array([1,2,3,4,5,6,7,8]).reshape(8,1))
 
     # Mutual information estimator
     mi = MutualInformationEstimator(
@@ -114,31 +112,54 @@ if __name__ == "__main__":
         torch.device("cpu"),
         noise_distr.compute_probability,
         data_distr.compute_probability,
-        np.array([-3,-1,1,3]).reshape(4,1)
+        np.array([1,2,3,4,5,6,7,8]).reshape(8,1)
     )
 
     # Start the training
-    accs = np.zeros((args.epochs,))
-    mutual_info = np.zeros((args.epochs,))
+    accs = list()
+    mutual_info_layer1 = list()
+    mutual_info_layer2 = list()
+    losses = list()
     for i in xrange(args.epochs):
         if (i+1) % 1 == 0:
-            # Convert to CPU
             m.eval()
             m.to(torch.device("cpu"))
             acc, test_out_noise, test_y = test_model(m, test_loader, torch.device("cpu"))
-            accs[i] = acc
+            accs.append(acc)
 
             # Get noise samples
-            gen_noise_outputs, gen_outputs = sample(m, torch.device("cpu"), num_samp=1000)
+            gen_noise_outputs, output_dict = sample(m, torch.device("cpu"), num_samp=1000)
             np.save(args.results_dir + "/epoch_{}_outputs_noise.npy".format(i+1), gen_noise_outputs.detach().cpu().numpy())
+            np.save(args.results_dir + "/epoch_{}_h1_noise.npy".format(i+1), output_dict["hidden_noise"].detach().cpu().numpy())
 
             # Compute MI
-            curr_mutual_info = mi.compute_mutual_information(gen_outputs["output"].detach().cpu().numpy(), 1000, "output", data_distribution_sampler=data_distr.sample, num_mc_samples=100)
-            mutual_info[i] = curr_mutual_info
+            curr_mutual_info_1 = mi.compute_mutual_information(
+                output_dict["hidden"].detach().cpu().numpy(),
+                1000,
+                "hidden",
+                data_distribution_sampler=data_distr.sample,
+                num_mc_samples=200
+            )
+            mutual_info_layer1.append(curr_mutual_info_1)
+            curr_mutual_info_2 = mi.compute_mutual_information(
+                output_dict["output"].detach().cpu().numpy(),
+                1000,
+                "output",
+                data_distribution_sampler=data_distr.sample,
+                num_mc_samples=200
+            )
+            mutual_info_layer2.append(curr_mutual_info_2)
 
-            print("Epoch {}; MI {}; Acc {}".format(i+1, curr_mutual_info, acc))
+            print("Epoch {}; Layer 1 MI {}; Layer 2 MI {}; Acc {}".format(
+                i+1,
+                curr_mutual_info_1,
+                curr_mutual_info_2,
+                acc)
+            )
+            if acc >= 0.99:
+                for g in optimizer.param_groups:
+                    g['lr'] = args.lr / 20.
 
-        # Convert back to original device
         m.train()
         m.to(device)
         for batch_idx, (data_x, data_y) in enumerate(train_loader):
@@ -148,23 +169,31 @@ if __name__ == "__main__":
             loss = loss_func(output_noise, data_y)
             loss.backward()
             optimizer.step()
+            #print("Loss val {}".format(loss.item()))
+            losses.append(loss.item())
 
 
     # Diagnostics
     for param in m.parameters():
         print("Weight:", param.data)
 
-    new_set_x, new_set_y = build_dataset(10)
-    model_y = np.tanh(6.9393*new_set_x - 14.2929)
+    new_set_x, new_set_y = build_dataset_eight(10)
+    model_y = -0.88*new_set_x + 4.3
+    model_y = np.maximum(model_y, model_y/10)
+    model_y = -0.23*model_y + 0.24
+    model_y = np.maximum(model_y, model_y/10)
+    print("New set x:", new_set_x)
     print("True out:", new_set_y.T)
     print("Model out:",  model_y.T)
-    new_set_x, new_set_y = build_dataset(10)
+    new_set_x, new_set_y = build_dataset_eight(10)
     print("Random out:", new_set_x.T)
     print("Random out label:", new_set_y.T)
 
     # Save stuff
-    np.save(args.results_dir + "/accuracies.npy", accs)
-    np.save(args.results_dir + "/mutual_information.npy", mutual_info)
+    np.save(args.results_dir + "/accuracies.npy", np.array(accs))
+    np.save(args.results_dir + "/mutual_information_layer1.npy", np.array(mutual_info_layer1))
+    np.save(args.results_dir + "/mutual_information_layer2.npy", np.array(mutual_info_layer2))
+    np.save(args.results_dir + "/losses.npy", np.array(losses))
 
 
 
