854b2a67ca66dc33551d123f4e435d09084f601f
Nathan Kong <kongnathan@gmail.com>
2019-04-07 09:01:37 -0700

    Minor

--- code/train_mc_sample_4.py
+++ code/train_mc_sample_4.py
@@ -47,7 +47,7 @@ def compute_acc(model_out, true_out):
 
 if __name__ == "__main__":
     # Sample size
-    N = 500
+    N = 25
 
     import argparse
     parser = argparse.ArgumentParser()
@@ -133,12 +133,13 @@ if __name__ == "__main__":
             np.save(args.results_dir + "/epoch_{}_outputs_noise.npy".format(i+1), gen_noise_outputs.detach().cpu().numpy())
 
             # Compute MI
-            curr_mutual_info = mi.compute_mutual_information(
-                gen_outputs["output"].detach().cpu().numpy(),
-                1000,
-                "output",
-                args.num_mc_samples
-            )
+#            curr_mutual_info = mi.compute_mutual_information(
+#                gen_outputs["output"].detach().cpu().numpy(),
+#                1000,
+#                "output",
+#                args.num_mc_samples
+#            )
+            curr_mutual_info = 0.0
             mutual_info[i] = curr_mutual_info
 
             print("Epoch {}; MI {}; Acc {}".format(i+1, curr_mutual_info, acc))
